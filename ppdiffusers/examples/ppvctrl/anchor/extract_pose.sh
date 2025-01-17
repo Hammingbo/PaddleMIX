7d925e2b26d3c9d9b9520d091368a73e73f97af6
Hammingbo <1257926092@qq.com>
2025-01-17 14:52:14 +0800

    extract_pose

--- ppdiffusers/examples/ppvctrl/anchor/extract_pose.sh
+++ ppdiffusers/examples/ppvctrl/anchor/extract_pose.sh
@@ -1,6 +1,6 @@
 python anchor/extract_pose.py  \
-       --det_model_dir=./anchor/pose_script/config/PP-YOLOE_plus-S_infer \
-       --keypoint_model_dir=./anchor/pose_script/config/paddle3.0_hrnet_w48_coco_wholebody_384x288/ \
+       --det_model_dir=./anchor/checkpoints/PP-YOLOE_plus-S_infer \
+       --keypoint_model_dir=./anchor/checkpoints/paddle3.0_hrnet_w48_coco_wholebody_384x288/ \
        --video_file=./examples/pose/case1/pixel_values.mp4  \
        --device=gpu \
        --output_dir=./examples/pose/case1/guide_values.mp4 \
